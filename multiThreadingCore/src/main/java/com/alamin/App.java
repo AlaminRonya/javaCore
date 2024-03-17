3b7428d850a2f0c6c41a794fbc498a09ec69fee4
AlaminRonya <hprony12@gmail.com>
2024-03-17 14:36:08 +0600

    Volatile keyword used

---
+++ multiThreadingCore/src/main/java/com/alamin/App.java
@@ -0,0 +1,7 @@
+package com.alamin;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+    }
+}
---
+++ multiThreadingCore/src/main/java/com/alamin/volatile_keyword/VisibilityExample.java
@@ -0,0 +1,25 @@
+package com.alamin.volatile_keyword;
+
+public class VisibilityExample {
+    private static boolean flag = false;
+
+    public static void main(String[] args) {
+        Thread writerThread = new Thread(() -> {
+            try {
+                Thread.sleep(1000); // Simulate some work
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            flag = true; // Set the flag to true
+            System.out.println("Flag is set to true.");
+        });
+
+        Thread readerThread = new Thread(() -> {
+            while (!flag);
+            System.out.println("Flag is true. Exiting.");
+        });
+
+        writerThread.start();
+        readerThread.start();
+    }
+}
